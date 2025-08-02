904c1fb133126eede2b002b33c3eac0617c8e65d
srishti-rao <srishtirao7@gmail.com>
2025-08-02 18:03:56 +0530

    Fix: remove main function

--- main.py
+++ main.py
@@ -45,26 +45,5 @@ def run_assistant():
         #else:
         #    speak("I didn't catch that. Please try again.")
 
-def main():
-    while True:
-        user_input = listen_command().lower()
-        print("Heard:", user_input)
-
-        for word in user_input.split():
-            cmd = match_command(word, commands)
-            if cmd == 'time':
-                from datetime import datetime
-                speak("Current time is " + datetime.now().strftime("%H:%M"))
-            elif cmd == 'reminder':
-                from tasks.reminder import set_reminder
-                set_reminder(10)
-                speak("Reminder set for 10 seconds.")
-            elif cmd == 'emergency':
-                speak("Calling emergency contact...")
-            elif cmd == 'weather':
-                speak("Weather feature is coming soon.")
-            else:
-                speak("Please say a valid command.")
-
 if __name__ == "__main__":
-    main()
+    show_gui(run_assistant)
