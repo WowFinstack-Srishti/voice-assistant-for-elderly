cfe5eb5c453439c65845d407d510f23a063d3d72
srishti-rao <srishtirao7@gmail.com>
2025-08-02 17:46:44 +0530

    Fix: update function

--- tasks/reminder.py
+++ tasks/reminder.py
@@ -1,8 +1,14 @@
 import threading
 import time
+import pyttsx3
 
-def set_reminder(seconds, message="Time to take medicine!"):
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def set_reminder(seconds, message):
     def remind():
         time.sleep(seconds)
-        print("Reminder:", message)
+        speak(message)
     threading.Thread(target=remind).start()
