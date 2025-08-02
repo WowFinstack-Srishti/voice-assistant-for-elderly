e9b3eced02a2c43b3247d722850f58cf9a1efb0f
srishti-rao <srishtirao7@gmail.com>
2025-08-02 17:43:01 +0530

    Fix: update soundex function

--- soundex_module.py
+++ soundex_module.py
@@ -1,15 +1,15 @@
 def soundex(word):
     word = word.upper()
     codes = {'BFPV': '1', 'CGJKQSXZ': '2', 'DT': '3', 'L': '4', 'MN': '5', 'R': '6'}
-    soundex_code = word[0]
+    result = word[0]
     for char in word[1:]:
         for key in codes:
             if char in key:
                 code = codes[key]
-                if code != soundex_code[-1]:
-                    soundex_code += code
-    soundex_code = soundex_code.ljust(4, '0')
-    return soundex_code[:4]
+                if result[-1]!= code:
+                    result += code
+    result = result.ljust(4, '0')
+    return result[:4]
 
 def match_command(input_word, commands):
     input_code = soundex(input_word)
